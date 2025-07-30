ea724ff308992f6d2d36b41b16b91a637ff06940
root <root@SLORN-B.localdomain>
2025-07-30 14:05:22 +0800

    add download.py and ipynb file

---
+++ rebelwayAppliedML/dataio/data_exploration/download_kaggle.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("valakhorasani/mobile-device-usage-and-user-behavior-dataset")
+
+print("Path to dataset files:", path)
