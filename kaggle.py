90264ca62817768f3b6c665a5f4477ad879bc300
LornS75 <1961341238@qq.com>
2026-01-14 14:19:49 +0800

    清理ML分支根目录：将业务文件迁移到ML子文件夹

--- kaggle.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-
-# Download latest version
-path = kagglehub.dataset_download("iabhishekofficial/mobile-price-classification")
-
-print("Path to dataset files:", path)
--- rebelwayAppliedML/dataio/data_exploration/download_kaggle.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("valakhorasani/mobile-device-usage-and-user-behavior-dataset")
-
-print("Path to dataset files:", path)
